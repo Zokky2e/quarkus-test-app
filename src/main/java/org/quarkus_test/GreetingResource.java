a0e47d17f633627b8a02a737624b56bbbd31e0a4
Zokky2e <icro4.gaming@hotmail.com>
2025-05-25 19:11:19 +0200

    renaming the namespace

--- src/main/java/org/quarkus_test/GreetingResource.java
+++ src/main/java/org/quarkus_test/GreetingResource.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.quarkus_test;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
--- src/test/java/org/quarkus_test/GreetingResourceIT.java
+++ src/test/java/org/quarkus_test/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.quarkus_test;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
